4a25689b4c899d27dfc023a2f675d89e1d743a30
uk14apyc <asheerali1997@gmail.com>
2025-05-26 01:54:46 +0200

    Refactor vite.config.ts for improved formatting and consistency

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,21 @@
 // vite.config.ts
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { build, defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   esbuild: {
-    jsx: 'automatic'
+    jsx: "automatic",
   },
   server: {
     host: true, // ensures Vite listens on all interfaces
     allowedHosts: [
-      'xcelbot-latest-r3m7.onrender.com', // Render's domain
-      'localhost'
-    ]
-  }
+      "xcelbot-latest-r3m7.onrender.com", // Render's domain
+      "localhost",
+    ],
+  },
+
+  build: {
+    outDir: "dist", // output directory for the build
+  },
 });
