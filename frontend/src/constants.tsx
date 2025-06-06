463049f8b9625c57ca4e853822c6025691932f7f
uk14apyc <asheerali1997@gmail.com>
2025-06-06 14:16:26 +0200

    fix: Update API_URL_Local to use the correct server address

--- frontend/src/constants.tsx
+++ frontend/src/constants.tsx
@@ -1,5 +1,5 @@
-export const API_URL_Local = "http://localhost:8000";
-// export const API_URL_Local = "http://3.149.94.190:8000";
+// export const API_URL_Local = "http://localhost:8000";
+export const API_URL_Local = "http://3.149.94.190:8000";
 // export const API_URL = 'http://localhost:8000/api/excel/upload';
 
 // import { API_URL_Local } from 'src/constants';
