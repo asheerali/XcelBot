e4e299a30e4a3d0745a340c88c0057743fd41bf0
uk14apyc <asheerali1997@gmail.com>
2025-08-12 14:17:37 -0400

    feat: Enhance delete_company function to remove associated mails, stores, and users

--- backend/crud/companies.py
+++ backend/crud/companies.py
@@ -1,6 +1,9 @@
 from sqlalchemy.orm import Session
+from models.users import User
+from models.mails import Mail
 from models.companies import Company
 from schemas.companies import CompanyCreate
+from models.locations import Store
 
 def create_company(db: Session, company: CompanyCreate) -> Company:
     # print("Creating company:", company)
@@ -26,10 +29,28 @@ def update_company(db: Session, company_id, company_data: CompanyCreate):
     db.refresh(db_company)
     return db_company
 
+# def delete_company(db: Session, company_id):
+#     db_company = get_company(db, company_id)
+#     if not db_company:
+#         return False
+#     db.delete(db_company)
+#     db.commit()
+#     return True
+
 def delete_company(db: Session, company_id):
     db_company = get_company(db, company_id)
     if not db_company:
         return False
+
+
+    # Delete all mails associated with this company first
+    db.query(Mail).filter(Mail.company_id == company_id).delete()
+
+    db.query(Store).filter(Store.company_id == company_id).delete()
+    
+    # also delete the user
+    db.query(User).filter(User.company_id == company_id).delete()
+
     db.delete(db_company)
     db.commit()
     return True
